efa69018c47a9fa5b20806b9ad9818a9b59d458c
Facundo Alvarado <facundoalvarado9@gmail.com>
2024-09-13 23:17:27 -0300

    añadidas referencias

--- laboratorio1.py
+++ laboratorio1.py
@@ -2,12 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 
+# Extensión del gráfico
+y_min, y_max = -2, 2
+x_min, x_max = -2, 2
+
+
 # Definir las cargas y sus posiciones
 cargas = [
     {'q': 1e-9, 'xq': 1, 'yq': 1},    # Carga positiva de 1 nC en (1, 1)
     {'q': -1e-9, 'xq': -1, 'yq': 1},  # Carga negativa de -1 nC en (-1, 1)
     {'q': 1e-9, 'xq': 0, 'yq': -1},   # Carga positiva de 1 nC en (0, -1)
-    {'q': -1e-9, 'xq': 0, 'yq': 0}    # Carga positiva de -1 nC en (0, 0)
+    {'q': -1e-9, 'xq': 0, 'yq': 0},    # Carga positiva de -1 nC en (0, 0)
 ]
 
 # Constantes
@@ -15,7 +20,7 @@ epsilon_0 = 8.854e-12  # Constante de permitividad eléctrica en el vacío (F/m)
 
 # Función para calcular el campo eléctrico debido a una carga puntual
 def campo_electrico(q, xq, yq, x, y):
-    k = 1 / (4 * np.pi * epsilon_0)  # Constante de Coulomb
+    k = 1 / (4 * np.pi * epsilon_0)  # Constante de Coulomb    
     r = np.sqrt((x - xq)**2 + (y - yq)**2)
     Ex = (k * q * (x - xq)) / (r**3)
     Ey = (k * q * (y - yq)) / (r**3)
@@ -29,8 +34,8 @@ def potencial_electrico(q, xq, yq, x, y):
     return V
 
 # Crear una malla de puntos en 2D
-x = np.linspace(-2, 2, 100)
-y = np.linspace(-2, 2, 100)
+x = np.linspace(y_min, y_max, 100)
+y = np.linspace(y_min, y_max, 100)
 X, Y = np.meshgrid(x, y)
 
 # Inicializar el campo eléctrico total
@@ -63,17 +68,20 @@ plot.streamplot(x, y, Ex_total, Ey_total, color=color, linewidth=1, cmap=plt.cm.
 # Generación de un punto para cada carga en gráfico de líneas de campo
 colores_carga = {True: '#aa0000', False: '#0000aa'}
 
-for carga in cargas:
+for indice, carga in enumerate(cargas):
     pos = carga['xq'], carga['yq']
-    plot.add_artist(Circle(pos, 0.05, color=colores_carga[carga['q']>0]))
+    ref = f"q{indice} = {carga['q']} en pos ({carga['xq']}, {carga['yq']})"
+    plot.add_artist(Circle(pos, 0.05, color=colores_carga[carga['q']>0], label=ref))
 
 # Graficar gráfico: Lineas de campo
 plot.set_title("Lineas de campo")
 plot.set_xlabel('$x$')
 plot.set_ylabel('$y$')
-plot.set_xlim(-2,2)
-plot.set_ylim(-2,2)
+plot.set_xlim(y_min,y_max)
+plot.set_ylim(y_min,y_max)
 plot.set_aspect('equal')
+plt.grid()
+plt.legend(loc="upper left")
 
 # Graficar gráfico: Potencial eléctrico
 plt.subplot(122)
@@ -83,4 +91,5 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.grid()
 plt.colorbar(label="Potencial (V)")
+
 plt.show()
